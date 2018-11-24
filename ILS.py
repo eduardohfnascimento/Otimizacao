1647eb337193fe6d81f76e631a72953a49abb9ac
Eduardo Henrique <eduardo.h.f@hotmail.com>
2018-11-24 20:24:43 -0200

    getting a valid solution

--- ILS.py
+++ ILS.py
@@ -4,16 +4,17 @@ import random
 def initialSolution(graph, vertex, kColor):
     solution = [-1] * int(vertex)
     solution[0] = random.randint(0, kColor - 1)
-    for x in range(1, int(vertex)):
-        edges = edgesFromVertex(graph, x)
-        colors = possibleColors(kColor)
-        for y in edges:
-            if (solution[int(y)] == -1):
-                continue
-            if solution[int(y)] in colors:
-                colors.remove(solution[int(y)])
-        if len(colors) == 0: solution[x] = random.randint(0, kColor - 1)
-        else : solution[x] = random.choice(colors)
+    while (min(solution) == -1):
+        for x in range(1, int(vertex)):
+            edges = edgesFromVertex(graph, x)
+            colors = possibleColors(kColor)
+            for y in edges:
+                if (solution[int(y)] == -1):
+                    continue
+                if solution[int(y)] in colors:
+                    colors.remove(solution[int(y)])
+            if len(colors) == 0: solution[x] = -1#random.randint(0, kColor - 1)
+            else : solution[x] = random.choice(colors)
 
 ## Todos os vértices que um vértice específico está ligado      
 def edgesFromVertex(graph, vertex):
@@ -24,4 +25,3 @@ def possibleColors (kColor):
     for i in range(0, kColor):
         colors.append(i)
     return colors
-    
